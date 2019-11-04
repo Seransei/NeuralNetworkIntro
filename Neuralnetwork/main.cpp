ee4a0a5faa857d2ddcbc2b961c4eaf71d6eefadd
Aloïs <marrocq.alois@laposte.net>
2019-11-04 15:07:32 +0100

    project with fann, training NN

--- Neuralnetwork/main.cpp
+++ Neuralnetwork/main.cpp
@@ -1,30 +1,34 @@
 ﻿#pragma once
 #include "include/fann.h"
+#include <string>
+#include <iostream>
+#include <fstream>
 
 void getNumInOut(const char*);
 
+const char* filename = "langue.data";
+
+unsigned int num_input;
+unsigned int num_output;
+
+
+const unsigned int num_layers = 3;
+const unsigned int num_neurons_hidden = 2;
+
+const float  desired_error = 0.001f;
+const unsigned int max_epochs = 500000;
+const unsigned int epochs_between_reports = 1000;
+
 int main()
 {
-	const char* filename = "langue.data";
-
-	unsigned int num_input;
-	unsigned int num_output;
-
 	getNumInOut(filename);
 
-	const unsigned int num_layers = 2;
-	const unsigned int num_neurons_layers = 5;
-
-	const float  desired_error = 0.001f;
-	const unsigned int max_epochs = 500000;
-	const unsigned int epochs_between_reports = 1000;
-
-	struct fann* ann = fann_create_standard(num_layers, num_input, num_neurons_layers, num_output);
+	struct fann* ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
 
 	fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
 	fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-	fann_train_on_file(ann, "xor.data", max_epochs, epochs_between_reports, desired_error);
+	fann_train_on_file(ann, filename, max_epochs, epochs_between_reports, desired_error);
 
 	fann_save(ann, "output.net");
 
@@ -35,5 +39,15 @@ int main()
 
 void getNumInOut(const char* filename) 
 {
+	std::ifstream in;
+	in.open(filename);
 
+	if (!in)
+		exit(1);
+
+	std::string buffer;
+	in >> buffer;
+
+	in >> num_input;
+	in >> num_output;
 }
